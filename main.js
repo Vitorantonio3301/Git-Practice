fb9b7acdc85a6daf0d51941dfe3b209b8a018e40
Vitorantonio3301 <Vitorantonion2003@gmail.com>
2023-05-16 20:35:08 -0300

    Js - Testing list and functions in JavaScript

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const list = [1, 2, 3, 4, 5, 6];
 
-function printlist(){
-    console.log(list());
-}
+for(counter = 0; counter < list.length; counter++){
+    if(list[counter] < list[5]){
+        console.log('O número é menor ou igual a 5');
+    }
 
-console.log(printlist());
+    else{
+        console.log('O número é maior que 5');
+    }
+}
 
 
 
